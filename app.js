8ba4ebb3f6ccaa4b008aacbf5bbe1e07689cb0f7
jkrusinski <jkruzr@gmail.com>
2017-02-17 11:38:29 -0600

    Completed tutorial

--- app.js
+++ app.js
@@ -1,1 +1,42 @@
-// TODO
+class GroceryListItem extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      bought: false
+    };
+  }
+
+  onListClick(event) {
+    this.setState({
+      bought: !this.state.bought
+    });
+  }
+
+  render() {
+
+    var style = {
+        textDecoration: this.state.bought ? 'line-through' : 'none',
+        color: this.state.bought ? 'black' : 'green'
+    };
+
+    return (
+      <li style={style} onClick={this.onListClick.bind(this)}>{this.props.item}</li>
+    );
+  }
+}
+
+
+var GroceryList = (props) => {
+
+  return (
+    <ul>
+      {props.list.map(item =>
+        <GroceryListItem item={item} />
+      )}
+    </ul>
+  );
+};
+
+ReactDOM.render(<GroceryList list={['Avocados', 'Petroleum Jelly', 'Eggplant']}/>, document.getElementById('app'));
